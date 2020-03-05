58ca93e09d7337c49c3b3e7d79ecb901db8aa7c6
esag99 <57884749+esag99@users.noreply.github.com>
2020-03-05 15:00:36 +0800

    Add files via upload
    vidio 2 dan 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require("electron");
 
-const {app, BrowserWindow, MEnu, ipcMain} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let todayWindow;
 let createWindow;
@@ -18,5 +18,78 @@ app.on("ready", ()=>{
     todayWindow.on("closed", ()=>{
         app.quit();
         todayWindow = null
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const listWindowCreator = ()=>{
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Apoinments"
     })
-})
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`)
+    listWindow.on("closed", ()=> (listWindow = null));
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Apoinments"
+    })
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`)
+    createWindow.on("closed", ()=> (createWindow = null));
+};
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    console.log(appointment);
+});
+
+
+const menuTemplate=[
+    {
+        label: "File",
+        submenu: [{
+           label: "New Appointment",
+
+           click(){
+               createWindowCreator();
+           }
+        },
+        {
+            label: "All Apointment",
+            
+            click(){
+                listWindowCreator();
+            }
+         },
+         {
+             label: "Quit",
+             accelerato: process.platform === "darwin" ? "Command+Q" : "Ctrl + Q",
+             click(){
+                app.quit()
+            }
+         }
+        
+    ]
+    },
+
+{
+    label: "View",
+    submenu: [{role: "reload"}, {role: "toogledevtools"}]
+}
+]
